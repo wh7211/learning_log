795242f18773780f7b79f831b1ae0cdcc868b3c9
Tyler <wh7211@gmail.com>
2019-09-07 17:42:57 +0800

    18.5 finished

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     # 主页
     # path('', views.index, name='index'),
     url(r'^$', views.index, name='index'),
+
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # 特定主题的详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
