d181a9e11872e4a7596371be76b33b5e03c266ab
Tyler <wh7211@gmail.com>
2019-10-15 15:55:11 +0800

    a

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,3 +135,24 @@ BOOTSTRAP3 = {
     'include_jquery': True,
     }
 
+# Heroku设置
+if os.getcwd() == '/app':
+    import dj_database_url
+
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # 让request.is_secure()承认X-Forwarded-Proto头
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # 支持所有的主机头(host header)
+    ALLOWED_HOSTS = ['*']
+
+    # 静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
