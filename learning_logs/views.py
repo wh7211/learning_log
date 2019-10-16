556629c1df6a735fe1dceabc2375487a50446c65
Tyler <wh7211@gmail.com>
2019-10-16 15:48:01 +0800

    a

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -28,7 +28,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
-    topic = Topic.objects.get(id=topic_id)
+    # topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
